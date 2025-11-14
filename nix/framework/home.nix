3a2e1aea006d74ea186e3a794676bdaadd3ab81d
Yusuf Bham <git@fiftysix.dev>
2025-11-14 00:37:18 -0500

    pipewire(framework): another fix
    Hopefully this one actually sticks?

--- nix/framework/home.nix
+++ nix/framework/home.nix
@@ -25,12 +25,18 @@ in
           default.clock.quantum = 4096;
           default.clock.min-quantum = 512;
           default.clock.max-quantum = 16384;
+
+          # man what the fuck
+          # https://www.reddit.com/r/linux_gaming/comments/1kk3ddo/simple_guide_fix_audio_crackling_or_sudden/
+          settings.clock.force-rate = 48000;
+          settings.clock.force-quantum = 500;
         };
       };
       "pipewire-pulse.conf.d"."pipewire-pulse.conf".source = util.toJSON {
         pulse.properties = {
           # Framework speaker has popping without this.
-          # Thank you to this reddit post for the solution
+          # Thank you to this reddit post for the solution (?)
+          # I had to add the stuff for pipewire.conf as well so not sure anymore lowkey
           # https://www.reddit.com/r/archlinux/comments/1kcns3u/pipewire_ungodly_crackling/mq4u6gs/
           pulse.min.quantum = "512/48000";
         };
